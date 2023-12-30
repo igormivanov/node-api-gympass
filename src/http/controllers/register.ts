64dc9d909446bf6a8670309e82c712d2079db126
igormivanov <igormivanovx@gmail.com>
2023-12-30 20:25:54 -0300

    refactor: modificando o acesso do repository

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { RegisterService } from '@/services/register'
-import { PrismaUsersRepository } from '../../repositories/prisma-users-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
