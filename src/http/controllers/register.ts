a24a11ffbaf7720034c4a70e19b96769831767fe
igormivanov <igormivanovx@gmail.com>
2023-12-30 20:59:07 -0300

    refactor: modificando o modo como é disparado o erro user-already-exists

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { RegisterService } from '@/services/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/services/errors/user-already-exists'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -18,7 +19,9 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerService.execute({ email, name, password })
   } catch (err) {
-    return reply.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
   }
 
   return reply.status(201).send()
