cca0f9284f34c1c4ae80f4c87c008fe84cd7572d
igormivanov <igormivanovx@gmail.com>
2024-01-09 15:36:50 -0300

    feat: implemento JWT no fastify

--- src/services/factories/make-create-gym-service.ts
+++ src/services/factories/make-create-gym-service.ts
@@ -1,7 +1,7 @@
 import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
 import { CreateGymService } from '../create-gym'
 
-export function makeValidateCheckInService() {
+export function makeCreateGymService() {
   const prismaGymsRepository = new PrismaGymsRepository()
   const createGymService = new CreateGymService(prismaGymsRepository)
 
